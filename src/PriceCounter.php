30dfea0885caa75f0a427732ac50424ea06f89cd
Tomas Pavlatka <tomas@pavlatka.cz>
2015-04-20 12:06:14 +0300

    Init Commit

---
+++ src/PriceCounter.php
@@ -0,0 +1,120 @@
+<?php
+
+class PriceCounter {
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Counts total amount for a product.
+     *
+     * @param array $product_data - data about product
+     * @param int $quantity - quantity
+     *
+     * @return array price data.
+     */
+    public function count_product_total($product_data, $quantity) {
+        // 1. Extract some needed variables.
+        $tax_percentage = $product_data['tax_percentage'];
+
+        // 2. Complete data for discount.
+        $product_data = $this->complete_discount_info($product_data);
+
+        // 3. Let's find default price.
+        $discount_total = 0;
+        $price = $product_data['price'];
+        if(!empty($product_data['discount_value'])) {
+            $discount_value = $product_data['discount_value'];
+
+            $price -= $discount_value;
+            $discount_total = $discount_value * $quantity;
+        }
+
+        // 4. Let's count prices.
+        $total = round($price * $quantity, 2);
+        $subtotal = round($total / (1 + ($tax_percentage / 100)), 2);
+        $tax_value = $total - $subtotal;
+
+        return array(
+            'total' => $total,
+            'subtotal' => $subtotal,
+            'tax' => $tax_value,
+            'discount' => $discount_total);
+    }
+
+    /**
+     * Counts total for all products.
+     *
+     * @param array $products_data - array with info for all products.
+     *
+     * @return array with price info.
+     */
+    public function count_total($products_data){
+        $total_price = array(
+            'total' => 0, 'subtotal' => 0, 'tax' => array(), 'discount' => 0);
+
+        foreach($products_data as $product_data) {
+            // Count total price foreach product.
+            $product_total = $this->count_product_total($product_data, $product_data['quantity']);
+
+            // Update total + subtotal.
+            $total_price['total'] += $product_total['total'];
+            $total_price['subtotal'] += $product_total['subtotal'];
+            $total_price['discount'] += $product_total['discount'];
+
+            // Update info about taxes.
+            $tax = $product_data['tax_percentage'];
+            if(!array_key_exists($tax, $total_price['tax'])) { // If we do not have this tax as key => create it.
+                $total_price['tax'][$tax] = 0;
+            }
+            $total_price['tax'][$tax] += $product_total['tax'];
+        }
+
+        return (array)$total_price;
+    }
+
+    /**
+     * Completes info about discount.
+     *
+     * @param array $product_data - data about product.
+     *
+     * @return array
+     */
+    public function complete_discount_info($product_data)
+    {
+        // Find discount.
+        $price = $product_data['price'];
+        $discount_value = $discount_percentage = null;
+
+        // If we have info about discount.
+        if (array_key_exists('discount', $product_data) && array_key_exists('discount_type', $product_data)) {
+            // Extract needed info into variables.
+            $discount = $product_data['discount'];
+            $discount_type = $product_data['discount_type'];
+
+            // If we have correct discount type and discount.
+            if(is_numeric($discount)) {
+                if($discount_type == 'fix') {
+                    // If discount type is fix, discount must be lower than price.
+                    if ($discount < $price) {
+                        $discount_value = $discount;
+                        $discount_percentage = round($discount / $price * 100, 2);
+                    }
+                } else if($discount_type == 'percentage') {
+                    if ($discount > 0 && $discount < 100) { // If type is percentage, discount must be between 0 and 100 (exl)
+                        $discount_value = round($price * $discount / 100, 2);
+                        $discount_percentage = $discount;
+                    }
+                }
+            }
+        }
+
+        // Complete array.
+        $product_data['discount_value'] = $discount_value;
+        $product_data['discount_percentage'] = $discount_percentage;
+
+        // Return it back.
+        return (array)$product_data;
+    }
+}
---
+++ tests/CounterTest.php
@@ -0,0 +1,210 @@
+<?php
+
+class PriceCounterTest extends PHPUnit_Framework_TestCase {
+
+    protected $PriceCounter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->PriceCounter = new PriceCounter();
+    }
+
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unset($this->PriceCounter);
+    }
+
+    /**
+     * @dataProvider data_testCountProductTotal
+     */
+    public function testCountProductTotal_Quantity_CorrectTotal($quantity, $expected)
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19);
+        $total_price = $this->PriceCounter->count_product_total($product_data, $quantity);
+
+        // Compare.
+        $this->assertEquals($total_price, $expected, $quantity);
+    }
+
+    public function testCompleteDiscountInfo_ProductWithFixValidDiscount_CorrectInfo()
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19,
+            'discount' => 20, 'discount_type' => 'fix');
+        $complete_data = $this->PriceCounter->complete_discount_info($product_data);
+
+        $expected = array(
+            'price' => 79.96,
+            'tax_percentage' => 19,
+            'discount' => 20,
+            'discount_type' => 'fix',
+            'discount_value' => 20,
+            'discount_percentage' => 25.01);
+
+        $this->assertEquals($complete_data, $expected);
+    }
+
+    public function testCompleteDiscountInfo_ProductWithFixInValidDiscount_CorrectInfo()
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19,
+            'discount' => 100, 'discount_type' => 'fix');
+        $complete_data = $this->PriceCounter->complete_discount_info($product_data);
+
+        $expected = array(
+            'price' => 79.96,
+            'tax_percentage' => 19,
+            'discount' => 100,
+            'discount_type' => 'fix',
+            'discount_value' => null,
+            'discount_percentage' => null);
+
+        $this->assertEquals($complete_data, $expected);
+    }
+
+    public function testCompleteDiscountInfo_ProductWithInValidDiscount_CorrectInfo()
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19,
+            'discount' => 100, 'discount_type' => 'fixx');
+        $complete_data = $this->PriceCounter->complete_discount_info($product_data);
+
+        $expected = array(
+            'price' => 79.96,
+            'tax_percentage' => 19,
+            'discount' => 100,
+            'discount_type' => 'fixx',
+            'discount_value' => null,
+            'discount_percentage' => null);
+
+        $this->assertEquals($complete_data, $expected);
+    }
+
+    public function testCompleteDiscountInfo_ProductWithPercentageValidDiscount_CorrectInfo()
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19,
+            'discount' => 20, 'discount_type' => 'percentage');
+        $complete_data = $this->PriceCounter->complete_discount_info($product_data);
+
+        $expected = array(
+            'price' => 79.96,
+            'tax_percentage' => 19,
+            'discount' => 20,
+            'discount_type' => 'percentage',
+            'discount_value' => 15.99,
+            'discount_percentage' => 20);
+
+        $this->assertEquals($complete_data, $expected);
+    }
+
+    public function testCompleteDiscountInfo_ProductWithPercentageInValidDiscount_CorrectInfo()
+    {
+        $product_data = array(
+            'price' => '79.96', 'tax_percentage' => 19,
+            'discount' => 100, 'discount_type' => 'percentage');
+        $complete_data = $this->PriceCounter->complete_discount_info($product_data);
+
+        $expected = array(
+            'price' => 79.96,
+            'tax_percentage' => 19,
+            'discount' => 100,
+            'discount_type' => 'percentage',
+            'discount_value' => null,
+            'discount_percentage' => null);
+
+        $this->assertEquals($complete_data, $expected);
+    }
+
+    public function testCountTotal_ProductsWithSameTax_CorrectTotal()
+    {
+        $products_data = array(
+            array(
+                'price' => '79.96', 'tax_percentage' => 19, 'quantity' => 5),
+            array(
+                'price' => '67.79', 'tax_percentage' => 19, 'quantity' => 10));
+        $total_price = $this->PriceCounter->count_total($products_data);
+
+        // Expected.
+        $expected = array(
+            'total' => 1077.7,
+            'subtotal' => 905.63,
+            'tax' => array(
+                19 => 172.07),
+            'discount' => 0);
+
+        // Compare.
+        $this->assertEquals($total_price, $expected);
+    }
+
+    public function testCountTotal_ProductsWithDifferentTaxes_CorrectTotal()
+    {
+        $products_data = array(
+            array(
+                'price' => '79.96', 'tax_percentage' => 19, 'quantity' => 5),
+            array(
+                'price' => '67.79', 'tax_percentage' => 5, 'quantity' => 10));
+        $total_price = $this->PriceCounter->count_total($products_data);
+
+        // Expected.
+        $expected = array(
+            'total' => 1077.7,
+            'subtotal' => 981.59,
+            'tax' => array(
+                5 => 32.28,
+                19 => 63.83),
+            'discount' => 0);
+
+        // Compare.
+        $this->assertEquals($total_price, $expected);
+    }
+
+    public function testCountTotal_ProductsWithDiscount_CorrectTotal()
+    {
+        $products_data = array(
+            array(
+                'price' => '79.96', 'tax_percentage' => 19, 'quantity' => 5, 'discount' => 20, 'discount_type' => 'fix'),
+            array(
+                'price' => '67.79', 'tax_percentage' => 19, 'quantity' => 10, 'discount' => 10, 'discount_type' => 'percentage'));
+        $total_price = $this->PriceCounter->count_total($products_data);
+
+        // Expected.
+        $expected = array(
+            'total' => 909.9,
+            'subtotal' => 764.62,
+            'tax' => array(
+                19 => 145.28),
+            'discount' => 167.8);
+
+        // Compare.
+        $this->assertEquals($total_price, $expected);
+    }
+
+    public function data_testCountProductTotal()
+    {
+        return array(
+            array(
+                1, array('total' => 79.96, 'subtotal' => 67.19, 'tax' => 12.77, 'discount' => 0)
+            ),
+            array(
+                5, array('total' => 399.8, 'subtotal' => 335.97, 'tax' => 63.83, 'discount' => 0)
+            ),
+            array(
+                10, array('total' => 799.60, 'subtotal' => 671.93, 'tax' => 127.67, 'discount' => 0)
+            ),
+            array(
+                50, array('total' => 3998, 'subtotal' => 3359.66, 'tax' => 638.34, 'discount' => 0)
+            ),
+            array(
+                100, array('total' => 7996.0, 'subtotal' => 6719.33, 'tax' => 1276.67, 'discount' => 0)
+            ),
+            array(
+                996, array('total' => 79640.16, 'subtotal' => 66924.50, 'tax' => 12715.66, 'discount' => 0)
+            )
+        );
+    }
+}
